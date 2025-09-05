1f23632f4330371aee49a387c2f2a0b7da17466b
Aditya Kumar Shrivastava <yashrivastava1785@gmail.com>
2025-09-06 00:27:23 +0530

    Add files via upload

---
+++ variable_example.java
@@ -0,0 +1,21 @@
+public class variable_example{
+    public static void main(String[] args){
+        int age = 20;
+        double pi = 3.1456;
+        char grade = 'A';
+        boolean is_student = true;
+        String name = "Yash";
+
+
+        System.out.println("age: " + age); 
+        System.out.println("pi: " + pi);        
+        System.out.println("grade: " + grade);
+        System.out.println("is_student: " + is_student);
+        System.out.println("name: " + name);
+
+
+
+
+        
+    }
+}
