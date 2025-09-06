91478464103345dbe096844461c4f68b23ddaf9b
Aditya Kumar Shrivastava <yashrivastava1785@gmail.com>
2025-09-06 12:19:07 +0530

    Add files via upload

---
+++ infocard.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class infocard {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter your collage name: ");
+        String collage = sc.nextLine();
+
+        System.out.println("Enter your hobby: ");
+        String hobby = sc.nextLine();
+
+         System.out.println("---------info card------------");
+        System.out.println("Name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("collage: " + collage );
+        System.out.println("hobby: " + hobby);
+
+
+        
+    }
+}
